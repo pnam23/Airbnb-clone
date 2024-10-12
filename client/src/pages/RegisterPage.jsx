0e5d71f7bae1d4e05618a70e5c32c861f6dcf2b4
Lê Nguyễn Phương Nam <97205910+pnam23@users.noreply.github.com>
2024-10-12 20:26:14 +0700

    Initial commit

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function RegisterPage(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <div className="flex items-center justify-around mt-4 grow">
+            <div className="mb-64">
+                <h1 className="mb-4 text-4xl text-center">Register</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="text" 
+                            placeholder="Phuong Nam" 
+                            value={name} 
+                            onChange={ev => setName(ev.target.value)}/>
+                    <input type="email" 
+                            placeholder="your@email.com" 
+                            value={email} 
+                            onChange={ev => setEmail(ev.target.value)}/>
+                    <input type="password" 
+                            placeholder="password"
+                            value={password} 
+                            onChange={ev => setPassword(ev.target.value)}/>
+                    <button className="primary">Register</button>
+                    <div className="py-2 text-center text-gray-500">
+                        Already have an account?
+                        <Link className="text-black underline" to={'/login'}>Login</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
